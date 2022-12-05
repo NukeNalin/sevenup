b07b149de1fcd2fcdbe4d8a83545d6df01dff65f
Nalin <nalinporwal10@gmail.com>
2022-12-05 23:38:34 +0530

    Initial Commit

---
+++ sevenup/sevenup.h
@@ -0,0 +1,18 @@
+//
+//  sevenup.h
+//  sevenup
+//
+//  Created by Nalin Porwal on 05/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sevenup.
+FOUNDATION_EXPORT double sevenupVersionNumber;
+
+//! Project version string for sevenup.
+FOUNDATION_EXPORT const unsigned char sevenupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sevenup/PublicHeader.h>
+
+
